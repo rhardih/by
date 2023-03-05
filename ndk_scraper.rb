6283a9a5f43a63cea33f9efd6ec26f89209169b7
René Hansen <renehh@gmail.com>
2023-03-05 21:58:31 +0100

    Update scraper
    This points it to the Github Wiki pages instead, as the old page that
    listed the NDKs is no longer valid.

--- ndk_scraper.rb
+++ ndk_scraper.rb
@@ -1,28 +1,70 @@
 require 'httparty'
 require 'nokogiri'
 
-NDK = Struct.new(:name, :version, :url, :sha1)
+NDK = Struct.new(:version, :url, :sha)
 ndks = []
 
-response = HTTParty.get("https://android-dot-devsite-v2-prod.appspot.com/ndk/downloads/older_releases_cf9eeb402fc5691b390be41c2c1f4a280be6ab60d99eb1606e63014b218845ad.frame")
+# Current LTS version
+current_url = 'https://github.com/android/ndk/wiki'
 
+response = HTTParty.get(current_url)
 document = Nokogiri.HTML(response.body)
-downloadsEl = document.css("body > div.all-downloads").first
 
-# All NDKs are flat-listed under this single parent element, but they each have
-# three associated elements; <h3>, <devsite-code> & <table>.
+h3_current_lts = document.xpath('//*[@id="wiki-body"]/div/h3[text() = "Current LTS Release"]')
+p_current_lts = h3_current_lts.xpath('following-sibling::p')
+table_current_lts = h3_current_lts.xpath('following-sibling::table')
 
-info = []
+_, col1, _, col3 = table_current_lts.xpath('tbody/tr[td[1]="Linux"]/td')
 
-filtered = downloadsEl.children.each do |node|
-  case node.name
-  when "h3"
-    ndks << NDK.new(node.text)
-  when "pre"
-    md = /\d+\.\d+\.\d+/.match(node.text)
-    ndks[-1].version = md[0]
-  when "table"
-    ndks[-1].url = node.xpath("tr[5]/td[2]/a/@href").first.value
-    ndks[-1].sha1 = node.xpath("tr[5]/td[4]").text
+current_lts_version = p_current_lts.first.text.split.first
+current_lts_url = col1.xpath('a/@href').first.value
+current_lts_sha = col3.text
+
+ndks << NDK.new(current_lts_version, current_lts_url, current_lts_sha)
+
+# Older versions
+older_url = 'https://github.com/android/ndk/wiki/Unsupported-Downloads'
+
+response = HTTParty.get(older_url)
+document = Nokogiri.HTML(response.body)
+
+# group = document.xpath('//*[@id="wiki-body"]/div/*[self::h3 or self::table]')
+h3_versions = document.xpath('//*[@id="wiki-body"]/div/h3')
+
+h3_versions.each do |h3_version|
+  version = h3_version.text
+
+  table = h3_version.xpath('following-sibling::table').first
+
+  if table.nil?
+    # The r10e and r9d are listed in an unordered list, without a sha, so they
+    # have been manually calculated and hardcoded here
+
+    missing_shas = {
+      'r10e' => "f692681b007071103277f6edc6f91cb5c5494a32",
+      'r9d' => "6d0cdb0b06eeafaa89890d05627aee89122b143f",
+    }
+
+    list = h3_version.xpath('following-sibling::ul')
+    item = list.xpath('li[starts-with(.,"Linux")]')
+    url = item.xpath('a/@href').first.value
+
+    if version == 'r9d'
+      # We don't want to support this version, because it's not packaged as a
+      # .zip file, which means we can't peak into it's contents as easily
+      next
+    end
+
+    ndks << NDK.new(version, url, missing_shas[version])
+  else
+    # Grab the url and sha from the table row that has Linux as platform
+    _, col1, _, col3 = table.xpath('tbody/tr[td[1]="Linux"]/td')
+
+    url = col1.xpath('a/@href').first.value
+    sha = col3.text
+
+    ndks << NDK.new(version, url, sha)
   end
 end
+
+pp ndks
